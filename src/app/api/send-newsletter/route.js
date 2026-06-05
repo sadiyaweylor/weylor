0b8ffa8c13c30bc3acb0956a12fe81cbcef7a056
Sadiyaweylor <sadiyaweylor@gmail.com>
2026-06-05 16:09:39 +0530

    Fix Supabase environment variables2

--- src/app/api/send-newsletter/route.js
+++ src/app/api/send-newsletter/route.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.SUPABASE_SERVICE_ANON_KEY
 );
 
 export async function POST(req) {
